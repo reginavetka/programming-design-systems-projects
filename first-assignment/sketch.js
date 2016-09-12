d8b3f736329d01edb869bef91653da8eb0a26edd
regve <reginavetka@Reginas-MacBook-Pro.local>
2016-09-12 13:58:16 -0400

    First Commit

--- first-assignment/sketch.js
+++ first-assignment/sketch.js
@@ -5,16 +5,57 @@ var r = new Rune({
   debug: true
 });
 
-r.rect(100, 100, 200, 200)
-  .fill(255, 0, 0)
-  .stroke(0)
 
-r.triangle(400, 50, 400, 400, 650, 50)
-  .fill(0, 255, 0)
-  .stroke(0)
 
-r.ellipse(200, 500, 200, 300)
-  .fill(0, 0, 255)
-  .stroke(false)
+//cone
+
+  r.triangle(350, 580, 420, 780, 450, 550)
+    .fill(0)   
+    .stroke(0)
+    .strokeWidth(2)
+    .rotate(-5);
+   
+
+// //balls
+
+var allBalls = 5;
+
+ for (var i = 0; i<allBalls ; i++) {
+  r.ellipse(400-i*(Math.random()*40+20), 500-i*50, 150+1*(Math.random()*10+1), 150+i*(Math.random()*2+1))
+     .fill(255)
+     .stroke(0)
+     .strokeWidth(1+i*0.7)
+     .rotate(-3)
+
+}
+
+
+
+//sprinkle rain
+
+ for (var i = 0; i<200 ; i++){
+
+ r.rect(700-i*(Math.random()*2+1), 100-i*(Math.random()*3+1), 1, 10)
+    .fill(0)
+    .stroke(0)
+
+
+// white sprinkles
+// for (var i = 0; i<250 ; i++){
+//  r.rect(700-i*(Math.random()*2+1)+100, 200-i*(Math.random()*3+1)+100, 1, 10)
+//     .fill(255)
+//     .stroke(255)
+
+
+    .rotate(Math.random() * (60) + 1)
+
+    }
+
+
+
+
+
+
+
 
 r.draw();
