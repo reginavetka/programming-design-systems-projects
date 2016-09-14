2c7382947c2282fd171851b0b9b884b14964fae0
reginavetka <rv861@nyu.edu>
2016-09-14 19:46:49 -0400

    Update sketch.js

--- first-assignment/sketch.js
+++ first-assignment/sketch.js
@@ -1,3 +1,4 @@
+
 var r = new Rune({
   container: "#canvas",
   width: 800,
@@ -22,8 +23,8 @@ var allBalls = 5;
 
  for (var i = 0; i<allBalls ; i++) {
   r.ellipse(400-i*(Math.random()*40+20), 500-i*50, 150+1*(Math.random()*10+1), 150+i*(Math.random()*2+1))
-     .fill(255)
-     .stroke(0)
+     .fill(0)
+     .stroke(255)
      .strokeWidth(1+i*0.7)
      .rotate(-3)
 
@@ -32,30 +33,15 @@ var allBalls = 5;
 
 
 //sprinkle rain
-
- for (var i = 0; i<200 ; i++){
-
- r.rect(700-i*(Math.random()*2+1), 100-i*(Math.random()*3+1), 1, 10)
-    .fill(0)
-    .stroke(0)
-
-
-// white sprinkles
-// for (var i = 0; i<250 ; i++){
-//  r.rect(700-i*(Math.random()*2+1)+100, 200-i*(Math.random()*3+1)+100, 1, 10)
-//     .fill(255)
-//     .stroke(255)
+for (var i = 0; i<250 ; i++){
+ r.rect(700-i*(Math.random()*2+1)+100, 200-i*(Math.random()*3+1)+100, 1, 10)
+    .fill(255)
+    .stroke(255)
 
 
     .rotate(Math.random() * (60) + 1)
 
     }
-
-
-
-
-
-
 
 
 r.draw();
